8b638fdb38248cd61aebc05588f52ef867b7c132
dmitrijs-rusovs <143439533+dmitrijs-rusovs@users.noreply.github.com>
2023-08-29 12:24:48 +0300

    Add files via upload

---
+++ 1-1-MNT-EL_SILT_AK-TES14-2.py
@@ -0,0 +1,34 @@
+import array as arr
+import numpy as np
+w1=[1,0.8,0]
+w2=[0,0.8,-1.0]
+w3=[0.5,0.5]
+e = arr.array('i',[0,0,0,0,1,1,1,1])
+s = arr.array('i',[0,0,1,1,0,0,1,1])
+a=  arr.array('i',[1,0,1,0,1,0,1,0])
+op= arr.array('i',[0,0,0,1,1,1,1,1])
+i=0
+def act(r):
+      return (1/(1+ np.exp(-r)))
+
+while i<8:
+
+        inputs=np.array([e[i],s[i],a[i]])
+        hid1=w1@inputs
+        b=act(hid1)
+        hid2=w2@inputs
+        c=act(hid2)
+        ot=np.array([b,c])@w3
+        out =act(ot)
+
+        if out>=0.6:
+              work=1
+        else:
+              work=0
+
+        if work==op[i]:      
+              print("i={},e={},s={},a={},op={},work={},true".format(i+1,e[i],s[i],a[i],op[i],work))        
+        else:
+              print("i={},e={},s={},a={},op={},work={},false".format(i+1,e[i],s[i],a[i],op[i],work))
+        i=i+1 
+
